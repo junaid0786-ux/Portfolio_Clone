fddb0f535059ce7c143bd30b67ab81201af4a121
Junaid Khan <junaid9068445457@gmail.com>
2025-11-10 01:11:54 +0530

    added javascript for fading animaton, added css transition on scroll

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll(".fade-in, .fade-left, .fade-right");
+
+  const appearOptions = {
+    threshold: 0.2,
+  };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+
+  faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
+  });
+});
